86afb19566f2276c0e89a749390b105c99eff67f
BlueEyesAndWhiteDragon <e1285209@u.nus.edu>
2024-01-29 14:43:22 +0800

    add some service

--- src/main/java/com/ad/teamnine/service/MemberService.java
+++ src/main/java/com/ad/teamnine/service/MemberService.java
@@ -1,7 +1,15 @@
 package com.ad.teamnine.service;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ad.teamnine.model.Member;
+import com.ad.teamnine.repository.MemberRepository;
+
 //import com.ad.teamnine.model.*;
 //import com.ad.teamnine.repository.*;
 import jakarta.transaction.Transactional;
@@ -9,10 +17,37 @@ import jakarta.transaction.Transactional;
 @Service
 @Transactional
 public class MemberService {
+
+	@Autowired
+	private MemberRepository memberRepo;
 	/*
-    @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
-    private UserRepository userRepository;
-*/
+	 * @Autowired private UserRepository userRepository;
+	 */
+
+	// update profile
+	public void updateMyProfile(Member member, String gender, Double height, Double weight, LocalDate birthdate) {
+		member.setGender(gender);
+		member.setHeight(height);
+		member.setWeight(weight);
+		member.setBirthdate(birthdate);
+		memberRepo.save(member);
+	}
+
+	// Register
+	public void register(String username, String password, String confirmPassword, String gender, Double height,
+			Double weight, LocalDate birthdate) {
+		if (password == confirmPassword) {
+			Member member = new Member();
+			member.setUsername(username);
+			member.setPassword(confirmPassword);
+			member.setGender(gender);
+			member.setHeight(height);
+			member.setWeight(weight);
+			member.setBirthdate(birthdate);
+			memberRepo.save(member);
+		}
+	}
+
+
+
 }
--- src/main/java/com/ad/teamnine/service/UserService.java
+++ src/main/java/com/ad/teamnine/service/UserService.java
@@ -1,7 +1,11 @@
 package com.ad.teamnine.service;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.ad.teamnine.model.Admin;
+import com.ad.teamnine.model.Member;
 import com.ad.teamnine.model.User;
 import com.ad.teamnine.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -10,10 +14,38 @@ import jakarta.transaction.Transactional;
 @Transactional
 public class UserService {
 	@Autowired
-	UserRepository userRepo; 
-	
-	//check for login
-	public boolean Login(User user) {
+	UserRepository userRepo;
+
+	// check for login
+	public boolean login(User user) {
+		if (user != null && user.getUsername() != null && user.getPassword() != null) {
+			if (userRepo.findByUsername(user.getUsername()).getPassword() == user.getPassword()) {
+				return true;
+			}
+		}
 		return false;
 	}
+	//Check identity
+	public boolean checkIfAdmin(User user){
+		    if(user instanceof Admin) 
+		    {     
+		     return true;    
+		    }
+		    else if (user instanceof Member) 
+		    {
+		     return false;          
+		    }   
+		    return false;
+	}
+	// delete
+	public void deleteAccount(int id) {
+		try {
+			userRepo.deleteById(id);
+			System.out.println("Account with ID " + id + " has been deleted");
+		} catch (EmptyResultDataAccessException e) {
+			System.out.println("Account with ID " + id + " does not exist");
+		}
+	}
+	
+	
 }
